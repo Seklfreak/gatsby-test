4121b86a3ac266a362d2ec992dd4cc534b69ee98
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-10-20 15:25:07 +0200

    tutorial part eight

---
+++ src/templates/blog-post.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div>
+          <span>Tags: </span>
+          {post.frontmatter.tags.map((item, index) => (
+            <span>
+              <b>{item}</b>
+              {index < post.frontmatter.tags.length - 1 ? ", " : ""}
+            </span>
+          ))}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        tags
+      }
+    }
+  }
+`
